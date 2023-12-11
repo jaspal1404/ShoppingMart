136ae975e9a9422e01aabeaca90cd811f12220da
jaspal1404 <jaspal.singh1404@gmail.com>
2023-12-11 11:40:16 -0600

    Update shopping.py

--- routers/shopping.py
+++ routers/shopping.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Path
 from pydantic import BaseModel, Field
 from starlette import status
-from models import Items, Cart
+from models import Items, Cart, Users
 from typing import Annotated
 from database import SessionLocal
 from sqlalchemy.orm import Session
@@ -43,6 +43,12 @@ class CartRequest(BaseModel):
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+@router.get("/get-all-items", status_code=status.HTTP_200_OK)
+async def get_all_items(db: db_dependency):
+
+    return db.query(Items).all()
+
+
 
 @router.get("/get-item", status_code=status.HTTP_200_OK)
 async def get_item_by_name(db: db_dependency, item_name: str, delivery_method: str, quantity: int):
@@ -53,7 +59,7 @@ async def get_item_by_name(db: db_dependency, item_name: str, delivery_method: s
     else:
         if item.quantity >= quantity:
             if item.delivery_method == delivery_method:
-                return {"response": True, "reason": "Item available & added to cart. Would you like to add another item ?", "id": item.id}
+                return {"response": True, "reason": "Item available & added to cart. Would you like to add another item ?"}
             else:
                 return {"response": False, "reason": "Item not available for " + delivery_method + ". Would you like to try a different delivery method ?"}
         else:
@@ -67,10 +73,41 @@ async def get_item_by_name(db: db_dependency, item_name: str, delivery_method: s
 @router.post("/add-item", status_code=status.HTTP_201_CREATED)
 async def add_item(db: db_dependency, item: ItemRequest):
 
-    item_model = db.query(Items).filter(Items.name == item.name).first()
+    item_model = db.query(Items).filter(Items.name == item.name).filter(Items.brand == item.brand).filter(Items.delivery_method == item.delivery_method).first()
     if item_model is None:
         item_model = Items(**item.model_dump())
         db.add(item_model)
         db.commit()
     else:
         raise HTTPException(status_code=404, detail="Item already exists")
+
+
+
+@router.post("/add-to-cart", status_code=status.HTTP_201_CREATED)
+async def add_to_cart(db: db_dependency, item_name: str, delivery_method: str, quantity: int, brand: str = ""):
+
+    if brand != "":
+        item = db.query(Items).filter(Items.name == item_name).filter(Items.delivery_method == delivery_method)\
+            .filter(Items.brand == brand).first()
+    else:
+        item = db.query(Items).filter(Items.name == item_name).filter(Items.delivery_method == delivery_method).first()
+
+    cart_item = Cart()
+    cart_item.name = item.name
+    cart_item.description = item.description
+    cart_item.brand = item.brand
+    cart_item.category = item.category
+    cart_item.quantity = quantity
+    cart_item.price = item.price
+    cart_item.item_id = item.id
+    cart_item.user_id = 1
+
+    db.add(cart_item)
+    db.commit()
+
+
+
+@router.get("/get-cart-items", status_code=status.HTTP_200_OK)
+async def get_cart_items(db: db_dependency):
+
+    return db.query(Cart).all()
